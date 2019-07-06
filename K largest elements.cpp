64343330da9a84d225c345e80b44b366127a381b
mayankwatts <47928731+mayankwatts@users.noreply.github.com>
2019-07-06 23:19:55 +0530

    Create K largest elements.cpp

---
+++ K largest elements.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+vector<int> kLargest(int input[], int n, int k){
+    
+    priority_queue<int,vector<int>,greater<int>> pq;
+    vector<int> v;
+    
+    for(int i=0;i<k;i++){
+        pq.push(input[i]);
+    }
+    
+    for(int i=k;i<n;i++){
+        if(input[i]>pq.top()){
+            pq.pop();
+            pq.push(input[i]);
+        }
+    }
+    
+    for(int i=0;i<k;i++){
+        v.push_back(pq.top());
+        pq.pop();
+    }
+    
+    return v;
+}
+
+
+int main() {
+	
+	int size;
+	cin >> size;
+	int *input = new int[1 + size];
+	
+	for(int i = 0; i < size; i++)
+		cin >> input[i];
+    
+    int k;
+    cin >> k;
+    
+    vector<int> output = kLargest(input, size, k);
+    for(int i = 0; i < output.size(); i++)
+        cout << output[i] << endl;
+	
+	return 0;
+}
